05c36e68aab1a1e062747624e7ac1ec68207a32d
Andreas Fink <andreas@fink.org>
2016-12-19 14:42:32 +0100

    removing core foundation where needed

--- ulibgsmmap/UMGSMMAP_RoutingInfoForSM_Arg.m
+++ ulibgsmmap/UMGSMMAP_RoutingInfoForSM_Arg.m
@@ -157,7 +157,6 @@
 }
 
 
-
 - (UMASN1Object<UMGSMMAP_asn1_protocol> *)decodeASN1opcode:(int64_t)opcode
                                              operationType:(UMTCAP_Operation)operation
                                              operationName:(NSString **)xop
