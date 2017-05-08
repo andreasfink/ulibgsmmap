ee6574bceb2af02210f4e2e6eb734ea53f0a77eb
Andreas Fink <andreas@fink.org>
2017-05-08 19:04:09 +0200

    osx packaging improvement

--- make_pkg.sh
+++ make_pkg.sh
@@ -7,7 +7,29 @@ PROJECT_NAME="$1"
 PKG_IDENTIFIER="$2"
 PKG_INSTALL_ROOT="$3"
 VERSION="$4"
-OUTPUT_FILE="$5"
+TIMESTAMP=`date +%Y%m%d%H%M`
 
-pkgbuild --root "$PKG_INSTALL_ROOT"  --install-location / --sign "${INSTALLER_CERT}" --version "${VERSION}" --identifier "${PKG_IDENTIFIER}"  "${OUTPUT_FILE}"
+if [ "" == "$5" ]
+then
+	OUTPUT_FILE="${PROJECT_NAME}-${VERSION}-${TIMESTAMP}.pkg"
+else
+	OUTPUT_FILE="$5"
+fi
+
+mkdir -p scripts
+F=scripts/preinstall
+echo "#!/bin/bash" > $F
+echo "VER=${VERSION}-${TIMESTAMP}" >> $F
+echo "MODULE=${PROJECT_NAME}" >> $F
+echo 'LATEST_VER=`curl --  "https://universalss7.ch/version.php?version=${VER}&os=macos&module=${MODULE}" 2> /dev/null `' >> $F
+echo "exit 0" >> $F
+chmod 755 $F
+
+if [ "${INSTALLER_CERT}" == "" ]
+then
+	pkgbuild --root "$PKG_INSTALL_ROOT"  --ownership recommended --install-location / ${CODESIGN} --scripts=scripts --version "${VERSION}" --identifier "${PKG_IDENTIFIER}"  "${OUTPUT_FILE}"
+else
+	pkgbuild --root "$PKG_INSTALL_ROOT"  --ownership recommended --install-location / ${CODESIGN} --scripts=scripts --sign "${INSTALLER_CERT}" --version "${VERSION}" --identifier "${PKG_IDENTIFIER}"  "${OUTPUT_FILE}"
+fi
+
 
