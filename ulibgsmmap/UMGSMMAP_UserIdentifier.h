8772eca06f35828e65611cc39d595cb1fee195c6
Andreas Fink <andreas@fink.org>
2017-12-13 17:11:06 +0100

    missing include

--- ulibgsmmap/UMGSMMAP_UserIdentifier.h
+++ ulibgsmmap/UMGSMMAP_UserIdentifier.h
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Andreas Fink (andreas@fink.org). All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
 #ifndef UMGSMMAP_UserIdentifier_defined
 #define UMGSMMAP_UserIdentifier_defined 1
