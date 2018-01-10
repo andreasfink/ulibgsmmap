c81da9a38457fc19c80469c50d844b689e8af0b8
Andreas Fink <afink@amdtelecom.net>
2018-01-10 16:35:22 +0200

    serialize

--- ulibgsmmap/UMLayerGSMMAP_Dialog.m
+++ ulibgsmmap/UMLayerGSMMAP_Dialog.m
@@ -275,10 +275,10 @@
     }
     if(self.logLevel <= UMLOG_DEBUG)
     {
-        NSMutableString *s = [[NSMutableString alloc]initWithFormat:@"queueMAP_Open_Ind"];
+        NSMutableString *s = [[NSMutableString alloc]initWithFormat:@"executeMAP_Open_Ind"];
         [self.logFeed debugText:s];
     }
-    [user queueMAP_Open_Ind:self.userIdentifier
+    [user executeMAP_Open_Ind:self.userIdentifier
                      dialog:self.userDialogId
                 transaction:localTransactionId
           remoteTransaction:remoteTransactionId
