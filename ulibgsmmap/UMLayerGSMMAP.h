ff593aefde58373cb262a2b59cfb6a4696ca33f3
Andreas Fink <afink@amdtelecom.net>
2018-02-01 09:35:24 +0200

    removed unused def

--- ulibgsmmap/UMLayerGSMMAP.h
+++ ulibgsmmap/UMLayerGSMMAP.h
@@ -90,13 +90,6 @@
                         result:(UMTCAP_asn1_Associate_result *)result
                     diagnostic:(UMTCAP_asn1_Associate_source_diagnostic *)result_source_diagnostic;
 
--(void) executeMAP_Delimiter_Req:(UMGSMMAP_DialogIdentifier *)dialogId
-                  callingAddress:(SccpAddress *)src
-                   calledAddress:(SccpAddress *)dst
-                         options:(NSDictionary *)options
-                          result:(UMTCAP_asn1_Associate_result *)result
-                      diagnostic:(UMTCAP_asn1_Associate_source_diagnostic *)result_source_diagnostic
-                        userInfo:(UMTCAP_asn1_userInformation *)userInfo;
 
 - (void)queueMAP_Close_Req:(UMGSMMAP_DialogIdentifier *)dialogId
        callingAddress:(SccpAddress *)src
