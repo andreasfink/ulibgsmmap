8759d5d455be0e63497a391b114b5529c2a0039c
Andreas Fink <afink@amdtelecom.net>
2019-01-10 12:44:01 +0200

    fix encoding of sendParamtersArg

--- ulibgsmmap/UMGSMMAP_SendParametersArg.m
+++ ulibgsmmap/UMGSMMAP_SendParametersArg.m
@@ -7,6 +7,8 @@
 //
 
 #import "UMGSMMAP_SendParametersArg.h"
+#import "UMGSMMAP_SubscriberIdentity.h"
+#import "UMGSMMAP_RequestParameterList.h"
 
 @implementation UMGSMMAP_SendParametersArg
 
@@ -20,4 +22,67 @@
     return self;
 }
 
+- (void) processBeforeEncode
+{
+	[super processBeforeEncode];
+	[_asn1_tag setTagIsConstructed];
+	_asn1_list = [[NSMutableArray alloc]init];
+	if(_subscriberId==NULL)
+	{
+		@throw([NSException exceptionWithName:@"PARAMETER_MISSING"
+									   reason:@"UMGSMMAP_SendParametersArg subscriberId missing"
+									 userInfo:NULL]);
+	}
+	[_asn1_list addObject:_subscriberId];
+	if(_requestParameterList==NULL)
+	{
+		@throw([NSException exceptionWithName:@"PARAMETER_MISSING"
+									   reason:@"UMGSMMAP_SendParametersArg requestParameterList missing"
+									 userInfo:NULL]);
+	}
+	[_asn1_list addObject:_requestParameterList];
+
+}
+
+
+- (UMGSMMAP_SendParametersArg *) processAfterDecodeWithContext:(id)context
+{
+	int p=0;
+	UMASN1Object *o = [self getObjectAtPosition:p++];
+	if(o)
+	{
+		_subscriberId = [[UMGSMMAP_SubscriberIdentity alloc]initWithASN1Object:o context:context];
+		o = [self getObjectAtPosition:p++];
+	}
+	if(o)
+	{
+		_requestParameterList = [[UMGSMMAP_RequestParameterList alloc]initWithASN1Object:o context:context];
+		o = [self getObjectAtPosition:p++];
+	}
+	return self;
+}
+
+
+
+- (NSString *) objectName
+{
+	return @"SendParametersArg";
+}
+
+- (id) objectValue
+{
+	UMSynchronizedSortedDictionary *dict = [[UMSynchronizedSortedDictionary alloc]init];
+	if(_subscriberId)
+	{
+		dict[@"subscriberId"] = _subscriberId.objectValue;
+	}
+	if(_requestParameterList)
+	{
+		dict[@"requestParameterList"] = _requestParameterList.objectValue;
+	}
+
+	return dict;
+}
+
 @end
+
